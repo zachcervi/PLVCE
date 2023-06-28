96fc54401b54279700e1f56361c8082dad7d9272
Zach Cervi <zachcervi@gmail.com>
2023-06-28 15:58:53 -0600

    Custom UI button and added button to Form

--- components/Places/PlaceForm.js
+++ components/Places/PlaceForm.js
@@ -3,12 +3,15 @@ import { View, Text, ScrollView, TextInput, StyleSheet } from "react-native";
 import { Colors } from "../../constants/colors";
 import ImagePicker from "./ImagePicker";
 import LocationPicker from "./LocationPicker";
+import Button from "../UI/Button";
 function PlaceForm() {
   const [enteredTitle, setEnteredTitle] = useState("");
 
   function changeTitleHandler(enteredText) {
     setEnteredTitle(enteredText);
   }
+
+  function savePlaceHandler() {}
   return (
     <ScrollView style={styles.form}>
       <View>
@@ -21,6 +24,7 @@ function PlaceForm() {
       </View>
       <ImagePicker />
       <LocationPicker />
+      <Button onPress={savePlaceHandler}>Add Place</Button>
     </ScrollView>
   );
 }
