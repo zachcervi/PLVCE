ce7839d643a2630a676ce8bf64b999098b20e5c2
Zach Cervi <zachcervi@gmail.com>
2023-06-28 15:02:15 -0600

    Map Preview with Location User button clicked
    location.js is in .gitignore

--- components/Places/PlaceForm.js
+++ components/Places/PlaceForm.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { View, Text, ScrollView, TextInput, StyleSheet } from "react-native";
 import { Colors } from "../../constants/colors";
 import ImagePicker from "./ImagePicker";
+import LocationPicker from "./LocationPicker";
 function PlaceForm() {
   const [enteredTitle, setEnteredTitle] = useState("");
 
@@ -19,6 +20,7 @@ function PlaceForm() {
         />
       </View>
       <ImagePicker />
+      <LocationPicker />
     </ScrollView>
   );
 }
